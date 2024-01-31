07286f187e43bd2b0f8b783225d3a306cb84c655
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-31 16:42:00 +0530

    Update ping.py

--- daxxop/modules/ping.py
+++ daxxop/modules/ping.py
@@ -10,22 +10,11 @@ from pyrogram.types import Message
 start_time = time.time()
 
 
-# █ ✪ █▓▓▓▓▓ [MRDAXX] ▓▓▓▓█ ✪ █#
+# █ ✪ █▓▓▓▓▓ [BADBABY] ▓▓▓▓█ ✪ █#
 
 PING_PIC = [
-    "https://telegra.ph/file/7c25ef427c9f3cded5577.jpg",
     "https://telegra.ph/file/625d235cc0a22fb8525b5.jpg",
-    "https://telegra.ph/file/1c62254d59baf7f968ba7.jpg",
-    "https://telegra.ph/file/7a0553bd4664486ab3008.jpg",
-    "https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg",
-    "https://telegra.ph/file/2773dec98d87b8562618c.jpg",
-    "https://telegra.ph/file/80353d02e0368b71d2666.jpg",
-    "https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg",
-    "https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg",
-    "https://telegra.ph/file/8371bcd8952d089f9ec05.jpg",
-    "https://telegra.ph/file/f970e559dd1bb96fced1a.jpg",
-    "https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"
-]
+    ]
 # ------------------------------------------------------------------------------- #
 
 
